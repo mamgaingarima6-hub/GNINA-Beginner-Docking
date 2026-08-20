fcf743733b67e6729482a63726c0ccd4050e7658
mamgaingarima6-hub <mamgain.garima6@gmail.com>
2026-08-20 17:07:48 +0530

    Add run_docking.sh script for molecular docking

---
+++ run_docking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RECEPTOR="input/receptor.pdbqt"
+LIGAND="input/ligand.pdbqt"
+OUTPUT="output/docked_poses.sdf"
+LOG="output/docking_log.txt"
+
+echo "Running GNINA molecular docking..."
+
+gnina \
+  --receptor $RECEPTOR \
+  --ligand $LIGAND \
+  --autobox_ligand $LIGAND \
+  --out $OUTPUT \
+  --log $LOG \
+  --exhaustiveness 8 \
+  --cpu 4
+
+echo "Docking completed successfully!"
